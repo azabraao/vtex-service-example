a27d0bdddb006c0f7dd67b0d2e750d509d3d4878
azabraao <az.abraao@gmail.com>
2021-01-14 18:09:35 -0300

    finish all vtex courses with a 404 status in API call but a happy smile in my face

--- node/middlewares/status.ts
+++ node/middlewares/status.ts
@@ -1,27 +1,12 @@
 export async function status(ctx: Context, next: () => Promise<any>) {
   const {
     state: { code },
-    clients: { status: statusClient },
+    clients: { catalog },
   } = ctx
 
-  console.info('Received code:', code)
+  const data = await catalog.getSkuById(code.toString())
 
-  const statusResponse = await statusClient.getStatus(code)
-
-  console.info('Status response:', statusResponse)
-
-  const {
-    headers,
-    data,
-    status: responseStatus,
-  } = await statusClient.getStatusWithHeaders(code)
-
-  console.info('Status headers', headers)
-  console.info('Status data:', data)
-
-  ctx.status = responseStatus
   ctx.body = data
-  ctx.set('Cache-Control', headers['cache-control'])
 
   await next()
 }
